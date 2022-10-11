c4c7f3b2aaebc82d9b8c134d7445478e8286920b
msanmaz <osanmaz4992@gmail.com>
2022-10-11 19:55:37 +0100

    /

--- pages/api/utils.js
+++ pages/api/utils.js
@@ -10,53 +10,6 @@ const generateFakeJob = (user) => ({
 })
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') return res.end()
-
-  if (req.body.task === 'clean_database') {
-    await prisma.job.deleteMany({})
-    await prisma.user.deleteMany({})
-  }
-
-  if (req.body.task === 'generate_users_and_jobs') {
-    let count = 0
-
-    while (count < 10) {
-      await prisma.user.create({
-        data: {
-          name: faker.internet.userName().toLowerCase(),
-          email: faker.internet.email().toLowerCase(),
-          company: faker.datatype.boolean(),
-        },
-      })
-      count++
-    }
-
-    //create 1 job for each user that's a company
-    const users = await prisma.user.findMany({
-      where: {
-        company: true,
-      },
-    })
-
-    users.forEach(async (user) => {
-      await prisma.job.create({
-        data: generateFakeJob(user),
-      })
-    })
-    res.send(users)
-  }
-
-  if (req.body.task === 'generate_one_job') {
-    const users = await prisma.user.findMany({
-      where: {
-        company: true,
-      },
-    })
-
-    await prisma.job.create({
-      data: generateFakeJob(users[0]),
-    })
-  }
-
+  res.send({name:"handan"})
   res.end()
 }
